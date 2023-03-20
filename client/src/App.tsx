dc39bed4110a37b9df8790db2241d51ac66533de
Jimmy Vu <JMV3211@gmail.com>
2023-03-20 12:07:30 -0700

    Set up foundations of the node server

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='font-bold'>Hello!</div>
+  )
+}
---
+++ server/index.js
@@ -0,0 +1,28 @@
+import * as dotenv from 'dotenv';
+import express from 'express';
+const app = express();
+dotenv.config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioPhoneNum = process.env.TWILIO_PHONE_NUM;
+console.log(twilioPhoneNum);
+import twilio from "twilio";
+const client = twilio(accountSid, authToken);
+
+app.post('/api/message', (req, res, next) => {
+  const { phoneNum, type } = req.body;
+
+  // client.messages
+  //   .create({
+  //     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+  //     from: `+${twilioPhoneNum}`,
+  //     to: `+${phoneNum}`
+  //   })
+  //   .then(message => console.log(message.sid));
+})
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Express listening on port ${process.env.PORT}`);
+});
