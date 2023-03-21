4e2ecead16f7027bb466e343821e485cc6ac2479
Jimmy Vu <JMV3211@gmail.com>
2023-03-21 16:17:01 -0700

    Installed eslint to the project

--- server/index.ts
+++ server/index.ts
@@ -5,20 +5,19 @@ dotenv.config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioPhoneNum = process.env.TWILIO_PHONE_NUM;
-console.log(twilioPhoneNum);
 import twilio from "twilio";
 const client = twilio(accountSid, authToken);
 
 app.post('/api/message', (req, res, next) => {
   const { phoneNum, type } = req.body;
 
-  // client.messages
-  //   .create({
-  //     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-  //     from: `+${twilioPhoneNum}`,
-  //     to: `+${phoneNum}`
-  //   })
-  //   .then(message => console.log(message.sid));
+  client.messages
+    .create({
+      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+      from: `+${twilioPhoneNum}`,
+      to: `+${phoneNum}`
+    })
+    .then(message => console.log(message.sid));
 })
 
 
