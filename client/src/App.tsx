bb9130e1458517e04b57a4530f05d7bb555fe409
Jimmy Vu <JMV3211@gmail.com>
2023-03-26 21:03:01 -0700

    Changed react router to use the new v6.9 routers and syntax

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,36 @@
 import React from 'react'
-import { createBrowserRouter, RouterProvider, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Vincent from './pages/Vincent'
-import Icarus from './pages/Icarus'
-import Header from './components/Header'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-]);
+import { createBrowserRouter, createRoutesFromElements, Route, Outlet, RouterProvider } from 'react-router-dom'
+import Home from './pages/home'
+import Vincent from './pages/vincent'
+import Icarus from './pages/icarus'
+import Header from './components/header'
+import ErrorPage from './pages/error'
 
 export default function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/vincent" element={<Vincent />} />
+        <Route path="/icarus" element={<Icarus />} />
+      </Route>
+    )
+  );
+
   return (
     <>
-      <RouterProvider router={router}></RouterProvider>
+      <RouterProvider router={router} />
     </>
   )
+}
+
+const Root = () => {
+  return (
+    <>
+      <Header />
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
 }
