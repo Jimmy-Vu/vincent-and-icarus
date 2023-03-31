c884de983632b9b1a06fc3c9c9882fa687930479
Jimmy Vu <JMV3211@gmail.com>
2023-03-31 09:23:44 -0700

    Set up foundation of header bar on mobile view

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header(props) {
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+  const menuStyle = {
+    closed: "w-full h-28 flex flex-row items-center px-5 shadow-md transition ease-in-out duration-700",
+    opened: "w-full h-80 flex flex-col items-center px-5 shadow-md transition ease-in-out duration-700"
+  }
   return (
-    <nav className="w-full bg-blue-500 text-white">
-      <ul className=" flex flex-row">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li className="ml-4">
-          <Link to="/vincent">Vincent</Link>
-        </li>
-        <li className="ml-4">
-          <Link to="/icarus">Icarus</Link>
-        </li>
-      </ul>
+    <nav className={isMenuOpen ? menuStyle.opened : menuStyle.closed}>
+      <div className="w-full h-28 flex flex-row justify-between items-center">
+        <Link className="text-4xl font-semibold" to="/">Vincent&Icarus</Link>
+        <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="w-12 h-12 border border-solid border-black rounded">
+          <i className="fa-sharp fa-solid fa-bars text-4xl"></i>
+        </button>
+      </div>
+      {isMenuOpen &&
+        <ul className="w-full flex flex-col items-center text-2xl text-center font-medium">
+          <li className="w-full border-b border-black pb-4 mb-4">
+            <Link to="/vincent">Meet Vincent</Link>
+          </li>
+          <li className="w-full border-b border-black pb-4 mb-4">
+            <Link to="/icarus"> Meet Icarus</Link>
+          </li>
+          <li className="w-44 h-12 flex justify-center items-center rounded bg-black text-white">
+            <Link  to="/get-started">Get Started</Link>
+          </li>
+        </ul>
+      }
     </nav>
   );
 }
