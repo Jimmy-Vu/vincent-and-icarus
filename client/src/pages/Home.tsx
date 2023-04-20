b76fbd5e72f3b66055b42d9a1bcb42c2afa68f60
Jimmy Vu <JMV3211@gmail.com>
2023-04-20 13:20:03 -0700

    Added main h1 for home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Home(props) {
   return (
     <>
-      <h1>Hello world!</h1>
+      <main className="w-full p-6">
+        <h1 className="text-3xl font-semibold">Stay balanced with Vincent&Icarus - text messages to keep your Icarus in check or uplift your Vincent.</h1>
+      </main>
     </>
   );
 }
