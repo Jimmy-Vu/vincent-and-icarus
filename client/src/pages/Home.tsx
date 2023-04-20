8392801dbe31330d53c73f358e6e2715f52c26ed
Jimmy Vu <JMV3211@gmail.com>
2023-04-20 13:36:34 -0700

    WIP. Inserted the main texts and image

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,8 +3,20 @@ import React from "react";
 export default function Home(props) {
   return (
     <>
-      <main className="w-full p-6">
-        <h1 className="text-3xl font-semibold">Stay balanced with Vincent&Icarus - text messages to keep your Icarus in check or uplift your Vincent.</h1>
+      <main className="w-full pt-10 px-6">
+        <h1 className="text-4xl font-semibold">Stay balanced with Vincent&Icarus - text messages to keep your Icarus in check or uplift your Vincent.</h1>
+        <ul className="mt-4">
+          <li className="mb-4 before:content-['>']">
+            Stay grounded with text reminders to keep your Icarus from flying too high
+          </li>
+          <li className="mb-4 before:content-['>']">
+            Uplift your spirits with personalized messages to channel your inner Vincent
+          </li>
+          <li className="mb-4 before:content-['>']">
+            Soar to new heights while staying grounded
+          </li>
+        </ul>
+        <img src="client/src/assets/Humaaans - Standing.png"/>
       </main>
     </>
   );
