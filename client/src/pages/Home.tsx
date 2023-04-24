a7d9c4225500dfed34c451e544f8afa7675b0c39
Jimmy Vu <JMV3211@gmail.com>
2023-04-24 12:09:30 -0700

    Completed text structure of home page content

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <>
-      <main className="w-full h-full flex flex-col">
-        <section id="1" className="max-h-screen pt-3 pb-5 px-6 flex flex-col items-center">
+      <main className="w-full h-full flex flex-col px-6 ">
+        <section id="1" className="max-h-screen pt-3 flex flex-col items-center">
           <div className="flex flex-col items-center">
             <h1 className="text-4xl font-semibold">Stay balanced with Vincent&Icarus - text messages to keep your Icarus in check or uplift your Vincent.</h1>
             <ul className="mt-4">
@@ -25,21 +26,30 @@ export default function Home() {
           </a>
         </section>
 
-        <section id="2" className="h-screen pt-28 pb-5 px-6 flex flex-col items-center">
-          <div className="my-10 text-center">
-            <p className="mb-3">Are you held back by <b>self-doubt or overconfidence</b>?</p>
-            <p className="mb-3">Struggling to balance your time and <b>achieve your goals</b>?</p>
-            <p className="mb-3"><b>Missed opportunities</b> due to hesitation or impulsiveness?</p>
-          </div>
-          <h2 className="text-3xl font-bold mb-10">Sound familiar?</h2>
-          <div className="mb-10 text-center">
-            <p className="mb-3">Feeling like you're ever good enough, or feeling like you can do no wrong?</p>
-            <p className="mb-3">Starting a project with great enthusiasm, only to become overwhelmed and lose motivation?</p>
-            <p className="mb-3">Feeling like your flaws hold you back, or feeling like you're invincible and can take on anything?</p>
-          </div>
-          <div>
-            <h1 className="text-3xl text-center font-bold mb-5">Meet your guidance counselors</h1>
-          </div>
+        <section id="2" className="h-fit pt-28 pb-5">
+          <section className="flex flex-col items-center">
+            <div className="my-10 text-center">
+              <p className="mb-3">Are you held back by <b>self-doubt or overconfidence</b>?</p>
+              <p className="mb-3">Struggling to balance your time and <b>achieve your goals</b>?</p>
+              <p className="mb-3"><b>Missed opportunities</b> due to hesitation or impulsiveness?</p>
+            </div>
+            <h2 className="text-3xl font-bold mb-10">Sound familiar?</h2>
+            <div className="mb-10 text-center">
+              <p className="mb-3">Feeling like you're ever good enough, or feeling like you can do no wrong?</p>
+              <p className="mb-3">Starting a project with great enthusiasm, only to become overwhelmed and lose motivation?</p>
+              <p className="mb-3">Feeling like your flaws hold you back, or feeling like you're invincible and can take on anything?</p>
+            </div>
+          </section>
+          <section className="flex flex-col items-center">
+            <h1 className="text-3xl text-center font-bold mb-5">Vincent and Icarus are here to guide you.</h1>
+            <div className="my-10 text-center">
+              <p className="mb-3">Vincent understands that some days are harder than others and that self doubt boggles us down from our true potential.</p>
+              <p className="mb-3">Icarus knows that that the thrill of success blind us, leading to us to take falls from grace.</p>
+            </div>
+            <Link className="w-52 h-14 flex justify-center items-center bg-black rounded hover:bg-neutral-800 hover:ring-slate-500" to="/get-started">
+              <button className=" text-white text-xl font-bold">Get Started</button>
+            </Link>
+          </section>
         </section>
       </main>
     </>
