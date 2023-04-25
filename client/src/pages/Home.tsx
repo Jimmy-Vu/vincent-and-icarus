86240687f83236bb94f24b91e781ceb826cb574f
Jimmy Vu <JMV3211@gmail.com>
2023-04-25 16:11:07 -0700

    Adjusted 2nd page's text size for large screens

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <main className="w-full h-full flex flex-col px-6 lg:px-48">
-        <section id="1" className="h-[calc(100vh-7rem)] pt-3 flex flex-col items-center">
+        <section id="home__main-page" className="h-[calc(100vh-7rem)] pt-3 flex flex-col items-center">
           <div className="h-full flex flex-col lg:flex-row items-center lg:justify-center">
             <div className="lg:basis-1/2">
               <h1 className="text-4xl lg:text-5xl font-semibold lg:font-bold">Stay balanced with Vincent&Icarus - text messages to keep your Icarus in check or uplift your Vincent.</h1>
@@ -25,27 +25,27 @@ export default function Home() {
               <img className="max-w-[160px] lg:max-w-[385px] aspect-[160/204]" src="client/src/assets/Humaaans - Standing.png" />
             </div>
           </div>
-          <a href="#2" className="absolute bottom-1 mx-auto text-3xl animate-bounce-slow">
+          <a href="#home-second-page" className="absolute bottom-1 mx-auto text-3xl animate-bounce-slow">
             <i className="fa-solid fa-chevron-down"></i>
           </a>
         </section>
 
-        <section id="2" className="h-screen  pt-28 -mt-28 pb-5">
+        <section id="home__second-page" className="h-screen pt-28 -mt-28 pb-5">
           <section className="flex flex-col items-center">
-            <div className="my-10 text-center">
+            <div className="my-10 text-center lg:text-xl">
               <p className="mb-3">Are you held back by <b>self-doubt or overconfidence</b>?</p>
               <p className="mb-3">Struggling to balance your time and <b>achieve your goals</b>?</p>
               <p className="mb-3"><b>Missed opportunities</b> due to hesitation or impulsiveness?</p>
             </div>
-            <h2 className="text-3xl font-bold mb-10">Sound familiar?</h2>
-            <div className="mb-10 text-center">
+            <h2 className="text-3xl lg:text-4xl font-bold mb-10">Sound familiar?</h2>
+            <div className="mb-10 text-center lg:text-xl">
               <p className="mb-3">Feeling like you're ever good enough, or feeling like you can do no wrong?</p>
               <p className="mb-3">Starting a project with great enthusiasm, only to become overwhelmed and lose motivation?</p>
               <p className="mb-3">Feeling like your flaws hold you back, or feeling like you're invincible and can take on anything?</p>
             </div>
           </section>
-          <section className="flex flex-col items-center">
-            <h1 className="text-3xl text-center font-bold mb-5">Vincent and Icarus are here to guide you.</h1>
+          <section className="flex flex-col items-center lg:text-xl">
+            <h1 className="text-3xl lg:text-4xl  text-center font-bold mb-5">Vincent and Icarus are here to guide you.</h1>
             <div className="my-10 text-center">
               <p className="mb-3">Vincent understands that some days are harder than others and that self doubt boggles us down from our true potential.</p>
               <p className="mb-3">Icarus knows that that the thrill of success blind us, leading to us to take falls from grace.</p>
