787a0cb6df7ceec77c77c7cb5f61965162f4a74f
Jimmy Vu <JMV3211@gmail.com>
2023-04-25 16:51:39 -0700

    Changed naming of the not found page

---
+++ client/src/pages/not-found.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div id="not-found-page">
+      <h1>Oops!</h1>
+      <p>The page you're looking is not found.</p>
+    </div>
+  );
+}
