7c50a6a0f66a273d8fd9bd86d0103ecf55b39e28
Jimmy Vu <JMV3211@gmail.com>
2023-04-29 18:02:32 -0700

    Adjusted eslint and tsconfig to not allow implicit anys and adjusted the semicolon rule

--- client/src/components/message-form-states/SecondStage.tsx
+++ client/src/components/message-form-states/SecondStage.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React from 'react'
 
-export default function TEMP(onNext) {
+export default function SecondStage (props) {
+  const { onNext, onBack } = props;
 
   return (
     <>
+      <h1>Second</h1>
+      <button onClick={onNext}>Next</button>
+      <button onClick={onBack}>Back</button>
     </>
-  );
+  )
 }
