5ea6a685f51e13fd53fc1857e00f0703cf9c9856
Jimmy Vu <JMV3211@gmail.com>
2023-05-03 10:57:35 -0700

    Style the archetype result stage

--- client/src/components/message-form-states/ArchetypeResultStage.tsx
+++ client/src/components/message-form-states/ArchetypeResultStage.tsx
@@ -19,9 +19,11 @@ export default function ArchetypeResultStage(props: messageFormStageProps): Reac
   }
   const result = '';
   return (
-    <div className="h-full flex flex-col">
-      <h1 className="pt-5 text-center text-xl font-semibold">{`Seems like you're ${message}`}</h1>
-      <p>Sounds about right? If so, let’s continue!</p>
+    <div className="h-full flex flex-col justify-between">
+      <section className="h-full flex flex-col justify-center">
+        <h1 className="pt-5 text-center text-xl font-semibold">{`Seems like you're ${message}`}</h1>
+        <p className="mt-5">Sounds about right? If so, let’s continue!</p>
+      </section>
       <div className="w-full flex justify-between mt-3">
         <button className="text-2xl" onClick={onBack}><i className="fa-solid fa-arrow-left"></i></button>
         <button className="text-2xl" onClick={onNext}><i className="fa-solid fa-arrow-right"></i></button>
