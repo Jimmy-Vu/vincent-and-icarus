573d698bf76914672451dc5b4b9eba65931e754e
Jimmy Vu <JMV3211@gmail.com>
2023-05-09 11:28:55 -0700

    Changed input validity and split the handleChange functions into two for easier comprehension

--- client/src/components/message-form-states/ArchetypeResultStage.tsx
+++ client/src/components/message-form-states/ArchetypeResultStage.tsx
@@ -22,7 +22,7 @@ export default function ArchetypeResultStage(props: messageFormStageProps): Reac
     <div className="h-full flex flex-col justify-between">
       <section className="h-full flex flex-col justify-center">
         <h1 className="pt-5 text-center text-xl font-semibold">{`Seems like you're ${message}`}</h1>
-        <p className="mt-5">Sounds about right? If so, let’s continue!</p>
+        <p className="mt-5">Sounds about right? If so, let&apos;s continue!</p>
       </section>
       <div className="w-full flex justify-between mt-3">
         <button className="text-2xl" onClick={onBack}><i className="fa-solid fa-arrow-left"></i></button>
--- client/src/components/message-form-states/InfoInputStage.tsx
+++ client/src/components/message-form-states/InfoInputStage.tsx
@@ -1,29 +1,47 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import type messageFormStageProps from "./interfaces/messageFormStageProps";
 
 export default function InfoInputStage(props: messageFormStageProps): React.ReactElement | null {
   const { onNext, onBack, setUserInfo, handleSubmit } = props;
 
-  if (props.userInfo === undefined) {
-    return null;
-  }
-  const { name, number } = props.userInfo;
-  const [consent, setConsent] = useState(false);
-  const [inputIsValid, setInputIsValid] = useState(true);
+  const [infoValidity, setInfoValidity] = useState({
+    name: false,
+    number: false,
+    consent: false
+  });
 
-  function handleChange(e: React.SyntheticEvent): void {
+  const [allIsValid, setAllIsValid] = useState(false);
+  useEffect(() => {
+    setAllIsValid(Object.values(infoValidity).every(currValue => currValue));
+  }, [infoValidity]);
+
+  function handleNameChange(e: React.SyntheticEvent): void {
     const target = e.target as HTMLInputElement;
     setUserInfo?.(prev => ({
       ...prev,
-      [target.name]: target.value
+      name: target.value
     }));
+
+    if (target.value.length !== undefined && target.value.length >= 2) {
+      setInfoValidity(prev => ({ ...prev, name: true }))
+    } else {
+      setInfoValidity(prev => ({ ...prev, name: false }))
+    }
   }
 
-  useEffect(() => {
-    if (name.length === 0 || number.length < 7) {
-      setInputIsValid(false);
+  function handleNumChange(e: React.SyntheticEvent): void {
+    const target = e.target as HTMLInputElement;
+    setUserInfo?.(prev => ({
+      ...prev,
+      number: target.value
+    }));
+
+    if (target.value.length !== undefined && target.value.length === 10) {
+      setInfoValidity(prev => ({ ...prev, number: true }))
+    } else {
+      setInfoValidity(prev => ({ ...prev, number: false }))
     }
-  }, [props.userInfo]);
+  }
 
   return (
     <div className="w-full">
@@ -33,23 +51,23 @@ export default function InfoInputStage(props: messageFormStageProps): React.Reac
           share your info with anyone else. And no need to stress - your information won&apos;t be stored on our servers either.</h1>
         <div className="flex flex-col">
           <div className="mb-5 flex flex-col">
-            <label htmlFor="name">Name</label>
-            <input onChange={handleChange} className="border border-solid border-black" type="text" name="name" id="name" />
+            <label htmlFor="name">First Name</label>
+            <input onChange={handleNameChange} className="border border-solid border-black" type="text" name="name" id="name"/>
           </div>
           <div className="mb-5 flex flex-col">
-            <label htmlFor="number">Phone Number</label>
-            <input onChange={handleChange} className="border border-solid border-black" type="tel" name="number" id="number" />
+            <label htmlFor="number">Phone Number (USA Only)</label>
+            <input onChange={handleNumChange} className="border border-solid border-black" type="tel" name="number" id="number" />
           </div>
           <div className="flex items-start mb-5">
-            <input onChange={() => { setConsent(prev => !prev); }} className="mt-1" type="checkbox" name="consent" id="consent" />
+            <input onChange={() => { setInfoValidity(prev => ({ ...prev, consent: !prev.consent })); }} className="mt-1" type="checkbox" name="consent" id="consent" />
             <label className="ml-1 text-sm" htmlFor="consent">I understand that Vincent&Icarus will use the information I provide to send me a personalized message. I consent to the name and phone number I provide for this purpose.</label>
           </div>
         </div>
       </section>
-      <p className={`ml-2 text-center text-red-600 ${inputIsValid ? '' : 'hidden'}`}>Please fill out all the required info.</p>
+      {/* <p className={`ml-2 text-center text-red-600 ${infoIsValid ? '' : 'hidden'}`}>Please fill out all the required info.</p> */}
       <div className="w-full flex justify-between mt-3">
         <button className="text-2xl" type="button" onClick={onBack}><i className="fa-solid fa-arrow-left"></i></button>
-        <button disabled={!consent} className={`text-2xl ${consent ? '' : 'disabled:opacity-50'}`} type="submit" onClick={handleSubmit}><i className="fa-solid fa-arrow-right"></i></button>
+        <button disabled={!allIsValid} className={`text-2xl ${allIsValid ? '' : 'disabled:opacity-50'}`} type="submit" onClick={handleSubmit}><i className="fa-solid fa-arrow-right"></i></button>
       </div>
     </div>
   );
