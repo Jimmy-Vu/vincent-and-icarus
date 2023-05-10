88e89654fbd8134e9f490b8afa9cf3b31b4f6ee6
Jimmy Vu <JMV3211@gmail.com>
2023-05-10 12:02:13 -0700

    Add initial data values

---
+++ database/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO "vincent" (
+  "message"
+)
+VALUES
+(
+  "I know it can be tough to stay motivated when you're feeling down, but remember that every small step counts towards your bigger goals. Keep pushing forward, one step at a time."
+),
+(
+  "Don't let self-doubt hold you back. You're capable of great things, even if you don't see it right now. Take a deep breath, believe in yourself, and go for it."
+)
