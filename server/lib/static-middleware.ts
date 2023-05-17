d2cecee3a2bd9ceeb9ff49852af592abf36488bc
Jimmy Vu <JMV3211@gmail.com>
2023-05-16 17:53:55 -0700

    Change static middleware to use vite's dist directory

---
+++ server/lib/static-middleware.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import path from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const distPath = path.join(__dirname, '../../dist');
+console.log(distPath);
+const staticMiddleware = express.static(distPath);
+
+export default staticMiddleware;
