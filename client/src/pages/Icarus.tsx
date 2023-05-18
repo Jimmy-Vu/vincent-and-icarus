7ff63dbbaa38f558ceacd5b0ae07a654660b6934
Jimmy Vu <JMV3211@gmail.com>
2023-05-18 13:26:10 -0700

    Add emoji representation to the h1 of the Meet pages

--- client/src/pages/Icarus.tsx
+++ client/src/pages/Icarus.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Icarus(): React.ReactElement {
   return (
-    <main className="w-full max-w-[1150px] h-full mt-8 px-20">
-      <h1 className="mb-4 text-5xl">Meet Icarus</h1>
+    <main className="w-full max-w-[1150px] h-full mt-8 px-8 lg:px-20">
+      <h1 className="mb-4 text-4xl lg:text-5xl">Meet Icarus 🕊</h1>
       <section className="text-lg">
         <p className="mb-5">Welcome to the Icarus page! Feeling confident and ready to take on the world? We&apos;ve all been there.
         But sometimes, our confidence can get the best of us, and that&apos;s where Icarus comes in to keep us from flying too close to the sun.</p>
