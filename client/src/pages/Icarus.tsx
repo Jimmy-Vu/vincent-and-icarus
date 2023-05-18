34173e609dc614aea02faad3320c80ac75642f62
Jimmy Vu <JMV3211@gmail.com>
2023-05-18 13:12:20 -0700

    Add Vincent page and Icarus page with styling

--- client/src/pages/Icarus.tsx
+++ client/src/pages/Icarus.tsx
@@ -2,6 +2,20 @@ import React from "react";
 
 export default function Icarus(): React.ReactElement {
   return (
-    <h1>Meet Icarus</h1>
+    <main className="w-full max-w-[1150px] h-full mt-8 px-20">
+      <h1 className="mb-4 text-5xl">Meet Icarus</h1>
+      <section className="text-lg">
+        <p className="mb-5">Welcome to the Icarus page! Feeling confident and ready to take on the world? We&apos;ve all been there.
+        But sometimes, our confidence can get the best of us, and that&apos;s where Icarus comes in to keep us from flying too close to the sun.</p>
+        <p className="mb-5">Icarus was a character from Greek mythology who flew too close to the sun, despite his father&apos;s warnings. His wings,
+        made of feathers and wax, melted, and he fell back to earth. But don&apos;t worry, we&apos;re not here to bring you down. In fact, we&apos;re here to help you soar!</p>
+        <p className="mb-5">With Icarus, you can embrace your confidence and ambition, but still keep your feet on the ground. Icarus reminds us that
+        there&apos;s a fine line between being confident and being overconfident, and he&apos;s here to help you stay on the right side of that line.</p>
+        <p className="mb-5">So, why did we choose Icarus to represent the mood of confidence and overconfidence? Because he knows what it&apos;s like to
+        want to reach for the stars, but also knows the importance of staying grounded. With Icarus on your side, you can shoot for the moon and achieve great
+        things, but you&apos;ll also be reminded to keep a level head and not let your ambition get the best of you.</p>
+        <p className="mb-5">So, go ahead and channel your inner Icarus. With his help, you can achieve anything you set your mind to, while still staying safe and grounded.</p>
+      </section>
+    </main>
   );
 }
