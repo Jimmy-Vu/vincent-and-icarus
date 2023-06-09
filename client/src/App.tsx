b7903c45d4f4acc38c08c97cdc5b36cf2df5bf34
Jimmy Vu <JMV3211@gmail.com>
2023-06-09 13:31:50 -0700

    Add scroll function to fire and shrink the header when the home page is scrolled pass the home screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { createBrowserRouter, createRoutesFromElements, Route, Outlet, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
 import Vincent from './pages/Vincent'
@@ -30,9 +30,34 @@ export default function App(): React.ReactElement {
 
 const Root = (): React.ReactElement => {
   const [isMiniHeader, setIsMiniHeader] = useState(false);
+  const headerValues = {
+    minHeight: 'h-24',
+    maxHeight: 'h-28',
+    minPaddingTop: 'mt-24',
+    maxPaddingTop: 'mt-28'
+  }
+
+  const handleScroll = (): void => {
+    const windowHeight = window.innerHeight;
+    const scrollValue = window.scrollY;
+    const isAtBottom = scrollValue >= windowHeight;
+
+    if (isAtBottom) {
+      setIsMiniHeader(true);
+    } else {
+      setIsMiniHeader(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [handleScroll])
+
   return (
     <div className="w-full h-full min-h-screen flex flex-col items-center">
-      <Header isMiniHeader={isMiniHeader} setIsMiniHeader={setIsMiniHeader} />
+      <Header isMiniHeader={isMiniHeader} setIsMiniHeader={setIsMiniHeader} headerValues={headerValues} />
       <div className="w-full max-w-screen-2xl h-full min-h-fit mb-6 pt-28 flex grow justify-center items-center">
         <Outlet />
       </div>
