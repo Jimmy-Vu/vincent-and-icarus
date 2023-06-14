a08ccb93c78584ec2e4cca8099ac068fded2e236
Jimmy Vu <JMV3211@gmail.com>
2023-06-13 19:58:44 -0700

    Add content to the About page

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -3,9 +3,25 @@ import React from "react";
 export default function About(): React.ReactElement {
   return (
     <main className="w-full max-w-[1150px] h-full mt-8 px-8 lg:px-20">
-      <h1 className="mb-4 text-4xl lg:text-5xl">About Us</h1>
+      <h1 className="mb-4 text-4xl lg:text-5xl">About V&I</h1>
       <section className="text-lg">
-        <p className="mb-5"></p>
+        <p className="mb-5">
+          First and foremost, thank you for your interest in Vincent&Icarus. The fact that you&apos;ve found your way to this little About page fills me with excitement and gratitude.
+        </p>
+        <p className="mb-5">
+          As I navigate through life, I&apos;ve realized the significance of balance and its constant presence in my journey. Balancing various aspects of life may seem simple in theory,
+          but it&apos;s often challenging to achieve. Nevertheless, like the vast universe, I believe that we are all striving to find equilibrium, and every effort towards it is a
+          positive step forward.
+        </p>
+        <p className="mb-5">
+          The inspiration for Vincent&Icarus stemmed from my personal experiences with self-doubt and moments of excessive confidence. These familiar struggles, of course, come with the territory
+          of existence. While self-doubt can be detrimental to our mental well-being, unchecked confidence can also lead to a fall.
+        </p>
+        <p className="mb-5">
+          Although Vincent&Icarus is still a work in progress, not yet at the scale and complexity I envision,
+          I&apos;m content with the progress made so far. Your decision to explore Vincent&Icarus and take the time to read this blurb means a lot. Once again, thank you sincerely for your support.
+          </p>
+        <p>- Jimmy</p>
       </section>
     </main>
   );
