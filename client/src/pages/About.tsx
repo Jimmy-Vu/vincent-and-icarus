26f4a9a31bb8dbf6131364c8b457ba51705bf509
Jimmy Vu <JMV3211@gmail.com>
2023-06-13 19:30:45 -0700

    Create the About component

---
+++ client/src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About(): React.ReactElement {
+  return (
+    <main className="w-full max-w-[1150px] h-full mt-8 px-8 lg:px-20">
+      <h1 className="mb-4 text-4xl lg:text-5xl">About Us</h1>
+      <section className="text-lg">
+        <p className="mb-5"></p>
+      </section>
+    </main>
+  );
+}
